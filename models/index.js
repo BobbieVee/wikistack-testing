6778888304b88e0ab259134ed53d1ffbd1f20877
Robert van der Mark <robert.vandermark@gmail.com>
2017-02-23 13:09:10 -0500

    testing through tag search

--- models/index.js
+++ models/index.js
@@ -94,9 +94,67 @@ var User = db.define('user', {
     }
 });
 
+const sync = ()=> {
+    return db.sync({force: true, logging: false});
+};
+
+const seed = ()=> {
+    const users = [
+        {
+            name: 'Bobby', 
+            email: 'robert.vandermark@gmail.com'
+        }, {
+            name: 'Sally Long-Tall', 
+            email: "RockAndRoll@music.com"
+        }
+    ];
+    const pages = [
+        {
+            title: 'Point of No Return',
+            urlTitle: 'point_of_no_return',
+            content: 'Ask not...',
+            userId: 1
+        }, 
+        {
+            title: 'Southern Man',
+            urlTitle: 'southern_man',
+            content: 'Keep your head',
+            userId: 2
+        },  
+        {
+            title: 'CarnEvil 9',
+            urlTitle: 'carnevil_9',
+            content: 'Welcome back my friends',
+            userId: 2
+        }
+    ];
+    const userPromises = users.map((user)=> {
+        return User.create(user);
+    });
+    const pagePromises = pages.map((user)=> {
+        return Page.create(user);
+    });
+
+    Promise.all(userPromises)
+    .catch((err)=> {
+        console.log('Error = ', err);
+    });
+    Promise.all(pagePromises)
+    .catch((err)=> {
+        console.log('Error = ', err);
+    });
+};
+
+
 Page.belongsTo(User, { as: 'author' });
+User.hasMany(Page);
+
+// sync();
+// seed();
 
 module.exports = {
     Page: Page,
-    User: User
+    User: User,
+    seed: seed,
+    sync: sync
 };
--- test/itsalive.js
+++ test/itsalive.js
@@ -4,31 +4,31 @@ const spies = require('chai-spies');
 const expect = chai.expect;
 chai.use(spies);
 
-describe('Testing functionality', ()=>{
-	it('confirms basic functionality', ()=> {
-		expect(2+2).to.equal(4);
-	});
+// describe('Testing functionality', ()=>{
+// 	it('confirms basic functionality', ()=> {
+// 		expect(2+2).to.equal(4);
+// 	});
 
-	it('it tests timeout\'s accuracy', (done)=> {
-		const start = new Date();
-		console.log('start = ', start);
-		setTimeout( ()=> {
-			const duration = new Date() - start;	
-			expect(duration).to.be.closeTo(1000, 50);
-			done();
-		}, 1000);
-	});
+// 	it('it tests timeout\'s accuracy', (done)=> {
+// 		const start = new Date();
+// 		console.log('start = ', start);
+// 		setTimeout( ()=> {
+// 			const duration = new Date() - start;	
+// 			expect(duration).to.be.closeTo(1000, 50);
+// 			done();
+// 		}, 1000);
+// 	});
 
-	it('Will invoke a function once per element', ()=> {
-		const arr = ['x', 'y', 'z'];
-		let logNth = (val, idx) => {
-			console.log('value at idx: '+idx+' = '+ val);
-		};
-		logNth = chai.spy(logNth);
-		arr.forEach(logNth);
-		expect(logNth).to.have.been.called.exactly(arr.length);	
-	});
+// 	it('Will invoke a function once per element', ()=> {
+// 		const arr = ['x', 'y', 'z'];
+// 		let logNth = (val, idx) => {
+// 			console.log('value at idx: '+idx+' = '+ val);
+// 		};
+// 		logNth = chai.spy(logNth);
+// 		arr.forEach(logNth);
+// 		expect(logNth).to.have.been.called.exactly(arr.length);	
+// 	});
 
 
 	
-});
+// });
